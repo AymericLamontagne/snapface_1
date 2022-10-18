6202cd42aa5d2ec1a0a889ad374245fb47c762e5
Aymeric Lamontagne <aymericlamontagne@gmail.com>
2022-10-18 08:07:58 -0400

    Part2 of behavior

--- awesome-components/src/app/app-routing.module.ts
+++ awesome-components/src/app/app-routing.module.ts
@@ -4,12 +4,24 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: 'social-media',
-    loadChildren: () => import('./social-media/social-media.module').then(
-      (m) => m.SocialMediaModule),
+    loadChildren: () =>
+      import('./social-media/social-media.module').then(
+        (m) => m.SocialMediaModule
+      ),
   },
   {
     path: 'complex-form',
-    loadChildren: () => import('./complex-form/complex-form.module').then(m => m.ComplexFormModule)
+    loadChildren: () =>
+      import('./complex-form/complex-form.module').then(
+        (m) => m.ComplexFormModule
+      ),
+  },
+  {
+    path: 'reactive-state',
+    loadChildren: () =>
+      import('./reactive-state/reactive-state.module').then(
+        (m) => m.ReactiveStateModule
+      ),
   },
   { path: '**', redirectTo: 'social-media' },
 ];
@@ -18,5 +30,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
